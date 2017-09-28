2c6d6c63990fa9f34fba2be22afe5bb7a84b40ad
baddala <baddala156@gmail.com>
2017-09-28 20:08:54 +0530

    28/9/17 wedding changes
    28/9/17 wedding changes

--- bookingCtrl.js
+++ bookingCtrl.js
@@ -8,29 +8,30 @@ app.controller("bookingCtrl", function ($scope,$http,$location,$window) {
 			});
 		
 		
-		if(!localStorage.getItem("user")){
+	/*	if(!localStorage.getItem("user")){
 			console.log(localStorage.getItem("user"));
 			console.log('hjhj');
 		}else{
 		//console.log(JSON.parse(localStorage.getItem("user")));
-	   $scope.user = $window.localStorage["user"];
-	  // $scope.user = JSON.parse($scope.user);
+	  // $scope.user = $window.localStorage["user"];
+	   $scope.user = JSON.stringify($scope.user);
 	   console.log(localStorage.getItem("user"));
 	   $scope.name = $scope.user.name;
 	   $scope.email = $scope.user.email;
 	   $scope.mobile = $scope.user.mobile;
+	   
 	  // localStorage.setItem("user", JSON.stringify($scope.user));				 
-	}
+	} */
 		
 
-/*	$http.get("get-session.php",{ params: { 'test': 'test' }})
+	$http.get("get-session.php",{ params: { 'test': 'test' }})
    .then(function (response) {
 	   $scope.user = response.data;
 	   $scope.name = $scope.user.name;
 	   $scope.email = $scope.user.email;
 	   $scope.mobile = $scope.user.mobile;
 	   //console.log(response.data);
-	}); */
+	}); 
    	
 	
 	
